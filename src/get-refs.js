8846b3cabee226b8cdbb9217c6dbe99d71e7d74d
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-10-08 15:12:47 +0300

    pars

--- src/get-refs.js
+++
@@ -1,8 +0,0 @@
-export default function getRefs() {
-    return {
-        searchForm: document.querySelector('#search-form'),
-        searchInput: document.querySelector('.search-input'),
-        searchBtn: document.querySelector('.search-botton'),
-        imagesContainer: document.querySelector('.gallery'),
-    }
-  }
