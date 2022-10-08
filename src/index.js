0b114047ebeba2149cdbd2a1f7d1a9c90e5fe6dc
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-10-08 15:02:34 +0300

    2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 import axios from 'axios';
 import getRefs from './get-refs';
 import renderMarkupImageInfo from './markup';
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
 
 
 const refs = getRefs();
@@ -81,30 +79,3 @@ async function  onSearch (e) {
       }
     }
 
-    const options = {
-  totalItems: 500,
-  itemsPerPage: 6,
-  visiblePages: 5,
-  page: 2,
-  centerAlign: true,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    //
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
-};
-const pagination = new Pagination('pagination', options);
